9bc33f63fa1fd2f05542eb18538b89d46247b85d
Kufreabasi <engineer_kufre@yahoo.com>
2022-08-08 18:25:01 +0100

    Created UI project and Guest interface

---
+++ RefitDemoUI/DataAccess/IGuestData.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using RefitDemoUI.DataAccess.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefitDemoUI.DataAccess
+{
+    public interface IGuestData
+    {
+        [Get("/Guests")]
+        Task<List<GuestDto>> GetGuests();
+
+        [Get("/Guests/{id}")]
+        Task<GuestDto> GetGuest(int id);
+
+        [Post("/Guests")]
+        Task CreateGuest([Body] GuestDto guest);
+
+        [Put("/Guest/{id}")]
+        Task UpdateGuest(int id, [Body] GuestDto guest);
+
+        [Delete("/Guests/{id}")]
+        Task DeleteGuest(int id);
+    }
+}
